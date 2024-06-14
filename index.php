494fbd740ad74f22463a011b2ff5f03aafa7147e
AmirMahmud03 <amirmhd030403@gmail.com>
2024-06-14 22:50:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+include 'functions.php';
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+    <br>
+    <img src="a.png" class="img-fluid" alt="...">
+    
+    <p align="justify">
+        <b>Profil Kampus</b><br>
+        Universitas Madura merupakan salah satu Perguruan Tinggi Swasta (PTS) di Pamekasan. Didirikan oleh Yayasan Universitas Madura (YUM) pada tanggal 02 Mei 1978. Saat ini Universitas Madura memiliki 6 fakultas dengan 13 program studi dan berkedudukan di Jalan Raya Panglegur KM. 3,5 Pamekasan.
+    </p>
+    
+    <p align="justify">
+        Kegiatan Universitas Madura dituangkan dalam Tri Dharma Perguruan Tinggi yang mencakup Pendidikan dan Pengajaran, Penelitian, serta Pengabdian pada Masyarakat. Organisasi Universitas Madura terdiri dari unsur pimpinan, unsur pelaksana, dan unsur kelengkapan universitas.
+    </p>
+    
+    <p align="justify">
+        Unsur pimpinan Universitas Madura adalah Rektor dan 3 (tiga) Wakil Rektor. Ketiga Wakil Rektor tersebut adalah Wakil Rektor Bidang Akademik (Wakil Rektor I), Wakil Rektor Bidang Administrasi dan Keuangan serta Bidang Kerjasama (Wakil Rektor II), dan Wakil Rektor Bidang Kemahasiswaan dan Alumni (Wakil Rektor III). Sedangkan unsur pelaksanan terdiri atas unsur akademik yang tersusun dari fakultas beserta program studi-program studi, unsur pelaksana teknis dan unsur pelaksana administratif. Unsur kelengkapan universitas adalah Senat Universitas.
+    </p>
+    
+    <p align="justify">
+        Pengelolaan proses pendidikan dan pengajaran mencakup berbagai kegiatan mulai dari perencanaan program pendidikan, penyiapan mahasiswa masuk dalam program pendidikan, pelaksana pendidikan dan pengajaran, pemantauan dan evaluasi proses belajar mengajar serta pengolahan datanya. Pelaksanaan kegiatan ini melibatkan berbagai unit dan unsur dalam sistem organisasi universitas dan seluruh sivitas akademika.
+    </p>
+    
+    <!-- Add more paragraphs as needed -->
+    
+</div>
+
+<?=template_footer()?>
